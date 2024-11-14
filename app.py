e3be4adf0ed46f12ee9e5bf4e66072b6eaffbfb1
PizzaEnjoyer1 <133069709+PizzaEnjoyer1@users.noreply.github.com>
2024-11-14 10:21:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -109,15 +109,9 @@ canvas_result = st_canvas(
 )
 
 ke = st.text_input('Ingresa tu Clave')
-#os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
-os.environ['OPENAI_API_KEY'] = ke
-
-
-# Retrieve the OpenAI API Key from secrets
-api_key = os.environ['OPENAI_API_KEY']
 
 # Initialize the OpenAI client with the API key
-client = OpenAI(api_key=api_key)
+client = OpenAI(api_key=ke)
 
 analyze_button = st.button("Crea tu historia", type="secondary")
 
